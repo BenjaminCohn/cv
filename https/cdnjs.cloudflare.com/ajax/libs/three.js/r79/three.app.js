424f64971eddba0509d4edf2b438108bec439562
BenjaminCohn <Benjamin.cohen@eemi.com>
2022-11-10 14:30:48 +0100

    cv

---
+++ https/cdnjs.cloudflare.com/ajax/libs/three.js/r79/three.app.js
@@ -0,0 +1,32 @@
+// Option 1: Import the entire three.js core library.
+import * as THREE from 'three'
+
+const scene = new THREE.Scene()
+
+
+// Option 2: Import just the parts you need.
+import { Scene } from 'three'
+
+const scene = new Scene()
+
+const scene = new THREE.Scene()
+const renderer = new THREE.WebGLRenderer()
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+
+const geometry = new THREE.CubeGeometry(5, 5, 20, 32);// <=fonctionne aussi avec le Cylinders
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true })
+const cylinder = new THREE.Mesh(geometry, material)
+
+scene.add(CubeGeometry)
+camera.position.z = 20
+
+renderer.setSize(window.innerWidth, window.innerHeight)
+document.body.appendChild(renderer.domElement)
+                          
+function animate() {
+    CubeGeometry.rotation.x += 0.01
+    CubeGeometry.rotation.y += 0.01
+    CubeGeometry.render(scene, camera)
+    requestAnimationFrame(animate)
+}
+animate()
